4e4c75ab6890f22dc6c7c2bbb20aee72645e108d
Galacs <34189296+Galacs@users.noreply.github.com>
2021-05-30 17:41:34 +0200

    Added download

---
+++ routes/download/download.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import mime from 'mime-types';
+
+import { S3Client, GetObjectCommand, GetObjectTaggingCommand } from '@aws-sdk/client-s3';
+
+const bucket = 'bucket';
+
+const client = new S3Client({
+  forcePathStyle: true,
+  endpoint: 'http://127.0.0.1:9000',
+  tls: false,
+  region: 'my-store',
+  credentials: {
+    accessKeyId: 'minioadmin',
+    secretAccessKey: 'minioadmin',
+  },
+});
+const router = express.Router();
+
+router.get('/download/:key', async (req, res) => {
+  try {
+    const data = await client.send(new GetObjectCommand({
+      Bucket: bucket,
+      Key: req.params.key,
+    }));
+    let filename = await client.send(new GetObjectTaggingCommand({
+      Bucket: bucket,
+      Key: req.params.key,
+    }));
+    filename = filename.TagSet.find((obj) => obj.Key === 'filename').Value;
+    res.writeHead(200, {
+      'Content-Disposition': `attachment; filename="${filename}"`,
+      'Content-Type': mime.contentType(filename) || 'application/octet-stream',
+      'Content-Length': data.ContentLength,
+    });
+    data.Body.pipe(res);
+  } catch (error) {
+    return res.status(404).json({ success: false, msg: 'Not found' });
+  }
+});
+
+export default router;
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import authLocal from './auth/local.js';
 import account from './account.js';
 import upload from './upload/upload.js';
+import download from './download/download.js';
 
 const router = express.Router();
 
 router.use('/auth/local', authLocal);
 router.use('/account', account);
 router.use('/upload', upload);
+router.use('/download', download);
 
 export default router;
