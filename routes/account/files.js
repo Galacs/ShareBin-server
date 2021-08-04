8e647f8d274c162ea719c7337ce16c730977281a
Galacs <34189296+Galacs@users.noreply.github.com>
2021-08-04 20:56:31 +0200

    Added file listing

---
+++ routes/account/files.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+
+const router = express.Router();
+const User = mongoose.model('User');
+
+router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const files = await User.findOne({ _id: jwt.decode(req.cookies.token).sub }, { objects: 1 });
+    console.log(files);
+    res.status(200).json(files.objects);
+  } catch (e) {
+    res.json({ success: false, msg: e });
+  }
+});
+
+export default router;
--- routes/account/index.js
+++ routes/account/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import deleteAccount from './delete.js';
+import files from './files.js';
 
 const router = express.Router();
 
 router.use('/', deleteAccount);
+router.use('/files', files);
 
 export default router;
