991f891e4f17eb119d8682aec86e8d624544b111
Galacs <34189296+Galacs@users.noreply.github.com>
2021-10-09 13:51:20 +0200

    tests

--- routes/auth/refresh.js
+++ routes/auth/refresh.js
@@ -1,21 +1,25 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import path from 'path';
+import fs from 'fs';
 
 import { issueJWT } from '../../lib/utils.js';
 
 const router = express.Router();
 const User = mongoose.model('User');
 
+const pathToKeyRefresh = path.join(path.resolve(), 'keys', 'id_rsa_pub_refresh.pem');
+const PUB_KEY_REFRESH = fs.readFileSync(pathToKeyRefresh, 'utf8');
+
 // Validate an existing user and issue a JWT
 router.get('/', async (req, res, next) => {
   try {
-    if (!req.cookies.refreshToken) return res.status(401).send('Unauthorized');
-    if (!jwt.verify(res.cookies.refreshToken)) return res.status(401).json({ msg: 'Expired refresh token' });
-
-    return res.status(403).json({ msg: 'No refresh token' });
-  // eslint-disable-next-line no-empty
-  } catch (e) {}
+    if (!req.cookies.refreshToken) return res.status(401).send('No refresh token');
+    if (!jwt.verify(req.cookies.refreshToken, PUB_KEY_REFRESH)) return res.status(401).json({ msg: 'Expired refresh token' });
+  } catch (e) {
+    return res.status(401).json({ msg: 'No refresh token' });
+  }
   try {
     const userId = jwt.decode(req.cookies.refreshToken).sub;
     const user = await User.findOne({ _id: userId }, { 'auth.local': 1 });
---
+++ tests/auth/refresh.test.js
@@ -0,0 +1,87 @@
+import supertest from 'supertest';
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
+import path from 'path';
+
+import app, { db } from '../../server.js';
+
+afterAll(async () => {
+  await db.disconnect();
+});
+
+describe('Testing local auth routes', () => {
+  const username = crypto.randomBytes(8).toString('base64');
+  const password = crypto.randomBytes(8).toString('base64');
+  let user;
+  let token;
+  let refreshToken;
+
+  it('Creating account', async () => {
+    await supertest(app).post('/auth/local/register')
+      .send({ username, password })
+      .expect(200, { success: true });
+
+    user = await db.model('User').findOne({ 'auth.local.username': username }, { 'auth.local': 1 });
+    expect(user).toBeTruthy();
+  });
+
+  it('Logging in', async () => {
+    await supertest(app).post('/auth/local/login')
+      .send({ username, password })
+      .expect(200, { success: true })
+      .expect(async (res) => {
+        // eslint-disable-next-line prefer-destructuring
+        token = res.headers['set-cookie'].find((obj) => obj.startsWith('token'))
+          .split('=')[1].split(';')[0];
+        // eslint-disable-next-line prefer-destructuring
+        refreshToken = res.headers['set-cookie'].find((obj) => obj.startsWith('refreshToken'))
+          .split('=')[1].split(';')[0];
+        // eslint-disable-next-line no-underscore-dangle
+        expect(JSON.stringify(user._id)).toBe(JSON.stringify(jwt.decode(token).sub));
+
+        const pathToKey = path.join(path.resolve(), 'keys', 'id_rsa_pub.pem');
+        const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
+        expect(jwt.verify(token, PUB_KEY)).toBeTruthy();
+      });
+  });
+
+  it('Testing protected route with token', async () => {
+    await supertest(app).get(`/user-protected/${jwt.decode(token).sub}`)
+      .set('Cookie', [`token=${token}`])
+      .expect(200, 'nice');
+  });
+
+  it('Testing refresh redirect', async () => {
+    await supertest(app).get(`/user-protected/${jwt.decode(token).sub}`)
+      .expect(302)
+      .expect(async (res) => {
+        if (res.statusCode === 302) {
+          await supertest(app).get('/auth/refresh')
+            .set('Cookie', [`refreshToken=${refreshToken}`])
+            .expect(302)
+            .expect(async (res1) => {
+              // eslint-disable-next-line prefer-destructuring
+              token = res1.headers['set-cookie'].find((obj) => obj.startsWith('token'))
+                .split('=')[1].split(';')[0];
+            });
+        }
+      });
+  });
+
+  it('Testing protected route with refreshed token', async () => {
+    await supertest(app).get(`/user-protected/${jwt.decode(token).sub}`)
+      .set('Cookie', [`token=${token}`])
+      .expect(200, 'nice');
+  });
+
+  it('Testing account deletion', async () => {
+    await supertest(app).delete('/account')
+      .set('Cookie', [`token=${token}`])
+      .send({ password })
+      .expect(200, { success: true, msg: 'User succesfully deleted' });
+
+    user = await db.model('User').findOne({ 'auth.local.username': username }, { 'auth.local': 1 });
+    expect(user).toBeFalsy();
+  });
+});
