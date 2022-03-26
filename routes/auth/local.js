fe4d04efde6ae1d22a9d3f3c09be8a6f35226707
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-03-26 14:39:33 +0100

    First route rewritten (auth.local)

--- routes/auth/local.js
+++ routes/auth/local.js
@@ -1,89 +1,80 @@
-import mongoose from 'mongoose';
 import express from 'express';
 
 import { issueJWT, genPassword, validPassword } from '../../lib/utils.js';
+import ObjectId from '../../lib/objectId.js';
+
+import pool from '../../config/database.js';
 
 const router = express.Router();
-const User = mongoose.model('User');
 
 // Validate an existing user and issue a JWT
-router.post('/login', (req, res, next) => {
-  User.findOne({ 'auth.local.username': req.body.username }, { 'auth.local': 1 })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ success: false, msg: 'could not find user' });
-      }
+router.post('/login', async (req, res) => {
+  try {
+    const data = await pool.query('SELECT * FROM auth.local WHERE username = $1', [req.body.username]);
+    if (data.rows.length === 0) {
+      return res.status(401).json({ success: false, msg: 'could not find user' });
+    }
+    if (validPassword(req.body.password, data.rows[0].password, data.rows[0].salt)) {
+      const tokenObject = issueJWT(data.rows[0].password.userid);
+      const refreshTokenObject = issueJWT(data.rows[0].password.userid, '31d', 2);
 
-      const isValid = validPassword(req.body.password, user.auth.local.hash, user.auth.local.salt);
-      if (isValid) {
-        const tokenObject = issueJWT(user);
-        const refreshTokenObject = issueJWT(user, '31d', 2);
+      // Create new Date instance
+      const date = new Date();
+      // Add time
+      const minutes = 15;
+      date.setMinutes(date.getMinutes() + minutes);
 
-        // Create new Date instance
-        const date = new Date();
-        // Add time
-        const minutes = 15;
-        date.setMinutes(date.getMinutes() + minutes);
+      // Create new Date instance
+      const refreshDate = new Date();
+      // Add time
+      refreshDate.setMonth(date.getMonth() + 1);
 
-        // Create new Date instance
-        const refreshDate = new Date();
-        // Add time
-        refreshDate.setMonth(date.getMonth() + 1);
-
-        res.cookie('refreshToken', refreshTokenObject.token, {
-          expires: refreshDate,
-          secure: false,
-          httpOnly: true,
-          path: '/auth/refresh',
-        });
-        res.cookie('token', tokenObject.token, {
-          expires: date,
-          secure: false,
-          httpOnly: true,
-        });
-        res.status(200).json({ success: true, username: req.body.username });
-      } else {
-        res.status(401).json({ success: false, msg: 'you have entered the wrong password' });
-      }
-    })
-    .catch((err) => {
-      next(err);
-    });
+      res.cookie('refreshToken', refreshTokenObject.token, {
+        expires: refreshDate,
+        secure: false,
+        httpOnly: true,
+        path: '/auth/refresh',
+      });
+      res.cookie('token', tokenObject.token, {
+        expires: date,
+        secure: false,
+        httpOnly: true,
+      });
+      res.status(200).json({ success: true, username: req.body.username });
+    } else {
+      res.status(401).json({ success: false, msg: 'you have entered the wrong password' });
+    }
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false });
+  }
 });
 
 // Register a new user
-router.post('/register', (req, res) => {
-  // Check if user already exists
-  User.findOne({ 'auth.local.username': req.body.username }, {})
-    .then((username) => {
-      if (!username) {
-        const saltHash = genPassword(req.body.password);
+router.post('/register', async (req, res) => {
+  const userid = ObjectId();
+  const saltHash = genPassword(req.body.password);
+  const { salt } = saltHash;
+  const { hash } = saltHash;
+  const client = await pool.connect();
 
-        const { salt } = saltHash;
-        const { hash } = saltHash;
-
-        const newUser = new User({
-          auth: {
-            local: {
-              username: req.body.username,
-              hash,
-              salt,
-            },
-          },
-        });
-
-        try {
-          newUser.save()
-            .then(() => {
-              res.json({ success: true });
-            });
-        } catch (err) {
-          res.json({ success: false, msg: err });
-        }
-      } else {
-        res.status(409).json({ success: false, msg: 'User already exists' });
-      }
-    });
+  try {
+    await client.query('BEGIN');
+    await client.query('INSERT INTO users(userid) VALUES ($1)', [userid]);
+    await client.query('INSERT INTO auth.local(userid, username, salt, password) VALUES ($1, $2, $3, $4)',
+      [userid, req.body.username, salt, hash]);
+    await client.query('COMMIT');
+    return res.json({ success: true });
+  } catch (e) {
+    if (e.code === '23505') {
+      await client.query('ROLLBACK');
+      return res.status(409).json({ success: false, msg: 'User already exists' });
+    }
+    console.log(e);
+    return res.json({ success: false });
+  } finally {
+    client.release();
+  }
 });
 
 export default router;
