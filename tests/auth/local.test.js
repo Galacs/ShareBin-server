6e178fa5f0965d752cc90b7002c8dbf20086e5be
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-03-26 17:16:36 +0100

    Updated first test suite

--- tests/auth/local.test.js
+++ tests/auth/local.test.js
@@ -4,11 +4,8 @@ import jwt from 'jsonwebtoken';
 import fs from 'fs';
 import path from 'path';
 
-import app, { db } from '../../server.js';
-
-afterAll(async () => {
-  await db.disconnect();
-});
+import app from '../../server.js';
+import pool from '../../config/database.js';
 
 describe('Testing local auth routes', () => {
   const username = crypto.randomBytes(8).toString('base64');
@@ -21,8 +18,8 @@ describe('Testing local auth routes', () => {
       .send({ username, password })
       .expect(200, { success: true });
 
-    user = await db.model('User').findOne({ 'auth.local.username': username }, { 'auth.local': 1 });
-    expect(user).toBeTruthy();
+    user = await pool.query('SELECT * FROM auth.local WHERE username = $1', [username]);
+    expect(user.rows[0]).toBeTruthy();
   });
 
   it('Testing local account creation with same name', async () => {
@@ -30,8 +27,8 @@ describe('Testing local auth routes', () => {
       .send({ username, password })
       .expect(409, { success: false, msg: 'User already exists' });
 
-    user = await db.model('User').findOne({ 'auth.local.username': username }, { 'auth.local': 1 });
-    expect(user).toBeTruthy();
+    user = await pool.query('SELECT * FROM auth.local WHERE username = $1', [username]);
+    expect(user.rows[0]).toBeTruthy();
   });
 
   it('Testing local account login', async () => {
@@ -43,7 +40,7 @@ describe('Testing local auth routes', () => {
         token = res.headers['set-cookie'].find((obj) => obj.startsWith('token'))
           .split('=')[1].split(';')[0];
         // eslint-disable-next-line no-underscore-dangle
-        expect(JSON.stringify(user._id)).toBe(JSON.stringify(jwt.decode(token).sub));
+        expect(JSON.stringify(user.rows[0].userid)).toBe(JSON.stringify(jwt.decode(token).sub));
 
         const pathToKey = path.join(path.resolve(), 'keys', 'id_rsa_pub.pem');
         const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
@@ -63,7 +60,7 @@ describe('Testing local auth routes', () => {
       .send({ password })
       .expect(200, { success: true, msg: 'User succesfully deleted' });
 
-    user = await db.model('User').findOne({ 'auth.local.username': username }, { 'auth.local': 1 });
-    expect(user).toBeFalsy();
+    user = await pool.query('SELECT * FROM auth.local WHERE username = $1', [username]);
+    expect(user.rows[0]).toBeFalsy();
   });
 });
