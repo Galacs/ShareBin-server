9aecfc2f7a81923db3182b995644b576350a9cb4
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-03-26 20:06:11 +0100

    Added prod env

--- config/database.js
+++ config/database.js
@@ -3,15 +3,27 @@ import pkg from 'pg';
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'sharebin',
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
+// eslint-disable-next-line import/no-mutable-exports
+let pool;
+
+if (process.env.NODE_ENV !== 'prod') {
+  pool = new Pool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'sharebin',
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  });
+} else {
+  pool = new Pool({
+    database: 'sharebin',
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  });
+}
 
 const sql = fs.readFileSync('sql/create_tables.sql').toString();
 
--- config/s3.js
+++ config/s3.js
@@ -13,13 +13,31 @@ export const bucket = 'bucket';
 //   },
 // });
 
-export default new S3Client({
-  forcePathStyle: true,
-  endpoint: 'http://127.0.0.1:9000',
-  tls: false,
-  region: 'my-store',
-  credentials: {
-    accessKeyId: 'minioadmin',
-    secretAccessKey: 'minioadmin',
-  },
-});
+// eslint-disable-next-line import/no-mutable-exports
+let client;
+
+if (process.env.NODE_ENV !== 'prod') {
+  client = new S3Client({
+    forcePathStyle: true,
+    endpoint: 'http://127.0.0.1:9000',
+    tls: false,
+    region: 'my-store',
+    credentials: {
+      accessKeyId: 'minioadmin',
+      secretAccessKey: 'minioadmin',
+    },
+  });
+} else {
+  client = new S3Client({
+    forcePathStyle: true,
+    endpoint: 'http://127.0.0.1:9000',
+    tls: false,
+    region: 'my-store',
+    credentials: {
+      accessKeyId: process.env.MINIO_USER,
+      secretAccessKey: process.env.MINIO_PASSWORD,
+    },
+  });
+}
+
+export default client;
