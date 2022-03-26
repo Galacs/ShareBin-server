ad7ad0d6d812ea09f20a20fe23ffb793926eaf1e
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-03-26 17:41:03 +0100

    Update node.js.yml

--- config/database.js
+++ config/database.js
@@ -3,25 +3,6 @@ import pkg from 'pg';
 
 const { Pool } = pkg;
 
-const dbCreatePool = new Pool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'postgres',
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
-
-dbCreatePool.query('CREATE DATABASE sharebin')
-  .catch((e) => {
-    if (e.code !== '42P04') {
-      console.log(e);
-    }
-  });
-
-dbCreatePool.end();
-
 const pool = new Pool({
   host: 'localhost',
   user: 'root',
