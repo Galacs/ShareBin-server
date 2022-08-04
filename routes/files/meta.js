9d0370c923604d0506db3a63a04c359b971ca8f2
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-08-04 22:18:03 +0200

    removed useless sql order

--- routes/files/meta.js
+++ routes/files/meta.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/:key', async (req, res) => {
   try {
-    const data = await pool.query('SELECT * FROM files WHERE ownerid = $1 AND fileid = $2 ORDER BY upload DESC', [jwt.decode(req.cookies.token).sub, req.params.key]);
+    const data = await pool.query('SELECT * FROM files WHERE ownerid = $1 AND fileid = $2', [jwt.decode(req.cookies.token).sub, req.params.key]);
     res.status(200).json(data.rows[0]);
   } catch (e) {
     res.json({ success: false, msg: e });
